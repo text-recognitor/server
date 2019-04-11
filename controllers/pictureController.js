f6412e8db3daeed57a17a54f0a5b9f03dfc31b61
paulinavita <paulinadavita@gmail.com>
2019-04-11 21:48:35 +0700

    integrate multer and gcp

--- controllers/pictureController.js
+++ controllers/pictureController.js
@@ -2,6 +2,13 @@ const axios = require("axios")
 
 class PictureController {
 
+static uploadFile(req, res) {
+    res.status(200).json({
+        link: req.file.cloudStoragePublicUrl
+    })
+
+    }
+
 }
 
 module.exports = PictureController
